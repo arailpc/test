57efd0589b56db934efe104fa109dc49fdce0a8d
arailpc <arailpc@protonmail.com>
2018-06-01 00:31:12 +0300

    .

--- hello.js
+++ hello.js
@@ -1,12 +1,11 @@
-/*eslint-env node*/
+const request = require('request');
+const fs = require('fs');
+const cheerio = require('cheerio');
 
-/*eslint-disable no-unused-params */
-var request = require('request');
-var fs = require('fs');
-var cheerio = require('cheerio');
+var URLlist = ['hello', 'word', 'sing'];
 
 function createURL(word) {
-   var url = `https://tatoeba.org/rus/sentences/search?query=${word}&from=eng&to=rus&orphans=&unapproved=&user=&tags=&list=&has_audio=yes&trans_filter=limit&trans_to=rus&trans_link=&trans_user=&trans_orphan=&trans_unapproved=&trans_has_audio=&sort=random`;
+   let url = `https://tatoeba.org/rus/sentences/search?query=${word}&from=eng&to=rus&orphans=&unapproved=&user=&tags=&list=&has_audio=yes&trans_filter=limit&trans_to=rus&trans_link=&trans_user=&trans_orphan=&trans_unapproved=&trans_has_audio=&sort=random`;
    //console.log(url);
    return {
        'url': url,
@@ -17,29 +16,58 @@ function createURL(word) {
 }
 
 function response_to_file(text) {
-    fs.writeFile('/projects/test/tmp', text, (err) => {
-        if (err) {
-            
-           return console.log('error writefile tmp');
+    fs.writeFile('./tmp', text, (err) => {
+        if (!err) {
+            console.log('file write');
+        } else {
+            return console.log('error writefile tmp');
         }
-        console.log('file write');
     });
 }
 
-function callback(error, response, body) {
-  if (!error && response.statusCode === 200) {
-      let html = body.toString();
-      response_to_file(html);
-      let $ = cheerio.load(html);
-      $('.sentence-and-translations').each((i, div) => {
-          let sentence = $(div).find('.sentence .text').text().trim();
-          let translation = $(div).find('.translations .text').first().text().trim();
-          console.log(sentence);
-          console.log(translation);
-      });
+function grab_page(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        let html = body.toString();
+        response_to_file(html);
+        let $ = cheerio.load(html);
+        $('.sentence-and-translations').each((i, div) => {
+            let sentence = $(div).find('.sentence .text').text().trim();
+            let translation = $(div).find('.translations .text').first().text().trim();
+            let id = $(div).find('a').first().text().trim().slice(1);
+            console.log(sentence);
+            console.log(translation);
+            console.log(id);
+            request('https://audio.tatoeba.org/sentences/eng/' +  id + '.mp3')
+                .pipe(fs.createWriteStream('mp3/' + id  + '.mp3'));
+            console.log(number++);
+        });
     }
 }
 
-var options = createURL('hello');
+function grab_some_page(url_list) {
+    let number = 0;
+    for (let i = 0; i < url_list.length; i++) {
+        console.log(url_list[i]);
+        let options = createURL(url_list[i]);
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                let html = body.toString();
+                response_to_file(html);
+                let $ = cheerio.load(html);
+                $('.sentence-and-translations').each((i, div) => {
+                    let sentence = $(div).find('.sentence .text').text().trim();
+                    let translation = $(div).find('.translations .text').first().text().trim();
+                    let id = $(div).find('a').first().text().trim().slice(1);
+                    console.log(sentence);
+                    console.log(translation);
+                    console.log(id);
+                    request('https://audio.tatoeba.org/sentences/eng/' +  id + '.mp3')
+                        .pipe(fs.createWriteStream('mp3/' + id  + '.mp3'));
+                    console.log(number++);
+                });
+            }
+        });
+    }
+}
 
-request(options, callback);
+grab_some_page(URLlist);
